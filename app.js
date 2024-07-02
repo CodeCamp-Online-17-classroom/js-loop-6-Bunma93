03e2b1465cc4ab59b9f1fe1d624772096fb8e719
Bunma93 <bunma93@gmail.com>
2024-07-02 10:32:10 +0700

    Js-loop6

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+let setNumber = +prompt("ใส่เลข 1 ตัว ที่มีค่าระหว่าง 1-100");
+let guess = +prompt("ลองทายดู");
+while (setNumber != guess) {
+    if (setNumber > guess){
+        alert("ตัวเลขมีค่ามากกว่านี้")
+    } else if (setNumber < guess){
+        alert("ตัวเลขมีค่าน้อยกว่านี้")
+    } 
+    guess = +prompt("ทายใหม่อีกครั้ง")
+};
+alert("ถูกต้องง");
